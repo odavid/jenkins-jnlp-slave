d16efa336cb75b40e460a21563b9bad318edbef6
Ohad David <Ohad.David@gmail.com>
2021-02-10 00:15:53 +0200

    Migrated travis-ci to github actions (#17)

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@ set -e -o pipefail
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-if [[ -n $TRAVIS_TAG ]]; then
-    TAG=${TRAVIS_TAG/v/}
+if [[ -n $GIT_TAG ]]; then
+    TAG=${GIT_TAG/v/}
     echo "publish $TAG"
 	docker tag odavid/jenkins-jnlp-slave odavid/jenkins-jnlp-slave:${TAG}
 	docker tag odavid/jenkins-jnlp-slave:alpine odavid/jenkins-jnlp-slave:${TAG}-alpine
